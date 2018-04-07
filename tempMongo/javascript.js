c975531835f78e29fea035cd3993607406c45f49
Jaw0608 <joseph.wilson@stonybrook.edu>
2018-04-07 19:40:13 -0400

    Mongo functions completed

--- tempMongo/javascript.js
+++ tempMongo/javascript.js
@@ -1,24 +1,51 @@
 let stitchClient;
+let stitchClientt;
 let appId = 'oneclickbudget-xueak';
+let appIdd= 'useroneclickbudget-vksnm'
 function pullCase(d) {
- d.count().then(count => {
-console.log(count)
- })
-}
-function get(d) {
 	d.find({}).execute().then(docs => {
 		console.log(docs)
+		console.log("Done")
 	})
 }
-function addCase(d) {
-	d.insertMany([{
-		spouse: '11111'
-	}, {spouse: '11110'}])
+function userPull(d) {
+	d.find({}).execute().then(docs => {
+		console.log(docs)
+		console.log("Done")
+	})
 }
-function editUserStats(d) {
+function editUserStats(d,spouse,job,children,house,balance,assests, expenses,transaction,day)
+ {
 	d.updateOne(
-		{"spouse":'True'}
+		{"status":spouse,
+		"employment": job,
+		"dependants": children,
+		"housing":house,
+		"balance": balance,
+		"assests": assests,
+		"expenses": expenses,
+		"transaction": [],
+		"day": 0
+	}
 	)
+}
+function addUser(d,spouse,job,children,house,balance, assests, expenses, transaction, day)
+ {
+	d.insertOne(
+		{"status":spouse,
+		"employment": job,
+		"dependants": children,
+		"housing":house,
+		"balance": balance,
+		"assests": assests,
+		"expenses": expenses,
+		"transaction": [],
+		"day": 0
+	}
+	)
+}
+function clearDebug(d) {
+	d.deleteMany({})
 }
 function initApplication() {
 	return stitch.StitchClientFactory.create(appId).then(client => {
@@ -32,11 +59,29 @@ function initApplication() {
 			coll.find({owner_id: stitchClient.authedId()}).limit(100).execute()
 		).then(docs => {
 			console.log("Found docs", docs)
-			console.log("[MongoDB Stitch]Connected to Stitch")
-			addCase(coll)
-			editUserStats(coll)
-			get(coll)
-			//pullCase(coll)
+			console.log("[MongoDB Stitch]Connected to Case App")
+			pullCase(coll)
+		}).catch(err => {
+			console.error(err)
+		});
+	});
+}
+function initUserApplication() {
+	return stitch.StitchClientFactory.create(appIdd).then(client => {
+		stitchClientt = client;
+	}).then(() => {
+		const dbb = stitchClientt.service('mongodb', 'mongodb-atlas').db('userOneBudget');
+		const colll = dbb.collection('user');
+		stitchClientt.login().then(() =>
+			colll.updateOne({owner_id: stitchClientt.authedId()}, {$set:{number:42}}, {upsert:true})
+		).then(()=>
+			colll.find({owner_id: stitchClientt.authedId()}).limit(100).execute()
+		).then(docs => {
+			console.log("Found docs", docs)
+			console.log("[MongoDB Stitch]Connected to User App")
+			clearDebug(colll)
+			addUser(colll, "Single", "Employed", "none", "none", 100, [{name: "Job", gain: 50.30, frequency: 7}], [{name: "Food", loss: 4.00, frequency: 1}], [],0)
+			userPull(colll)
 		}).catch(err => {
 			console.error(err)
 		});
